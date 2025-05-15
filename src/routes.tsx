9d0302b5bf111d6884dea53b9253d98f45dced4a
anticheat131 <wowsamowow@gmail.com>
2025-05-15 18:22:30 +0200

    routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -27,7 +27,7 @@ const TermsOfService = lazy(() => import('./pages/TermsOfService'));
 const ContentRemoval = lazy(() => import('./pages/ContentRemoval'));
 const DMCANotice = lazy(() => import('./pages/DMCANotice'));
 
-<Route path="/anime" element={<Anime />} />
+
 export default function AppRoutes() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
