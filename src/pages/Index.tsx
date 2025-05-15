3c47ae719a6ea423934ecd12a31627a17a0c8c1a
anticheat131 <wowsamowow@gmail.com>
2025-05-15 04:45:49 +0200

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -123,6 +123,15 @@ const Index = () => {
           </div>
         </>
       )}
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PTPYEBLBGN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PTPYEBLBGN');
+</script>
 
       <Footer />
     </main>
