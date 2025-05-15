022411b035ffd0965b99e786803171011b7789b4
anticheat131 <wowsamowow@gmail.com>
2025-05-15 04:53:22 +0200

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -123,16 +123,7 @@ const Index = () => {
           </div>
         </>
       )}
-      <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PTPYEBLBGN"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-PTPYEBLBGN');
-</script>
-
+      
       <Footer />
     </main>
   );
