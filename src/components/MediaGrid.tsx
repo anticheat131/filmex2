4f51b43a418f5109de73bdeb76aef106f2156ec5
anticheat131 <wowsamowow@gmail.com>
2025-05-16 16:31:37 +0200

    MediaGrid.tsx

--- src/components/MediaGrid.tsx
+++ src/components/MediaGrid.tsx
@@ -206,7 +206,7 @@ const MediaGrid = ({
         </motion.div>
       ) : (
         <motion.div
-          className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4"
+          className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-[3.5rem]"
           variants={container}
           initial="hidden"
           animate="show"
