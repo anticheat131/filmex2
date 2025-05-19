e63b63d5980c52ac409e51446a10268ab17595ef
anticheat131 <wowsamowow@gmail.com>
2025-05-19 09:04:18 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -330,7 +330,7 @@ export default defineConfig(({ mode }) => ({
         }
       },
       devOptions: {
-        enabled: true,
+        enabled: false,
         type: 'module',
         navigateFallback: 'index.html'
       }
