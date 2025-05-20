d495e4cf3734ede33dc0cb6dedf1d127fecfe43f
anticheat131 <wowsamowow@gmail.com>
2025-05-20 10:18:16 +0200

    Update Logo.tsx

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -14,39 +14,12 @@ const Logo = () => {
       }}
     >
       <span
-        className="text-3xl text-white tracking-wide relative"
+        className="text-3xl tracking-wide text-white"
         style={{ letterSpacing: '0.05em' }}
       >
         Filme
         <span className="text-indigo-500">X</span>
-        <span
-          aria-hidden="true"
-          className="absolute inset-0 bg-gradient-to-r from-transparent via-white/50 to-transparent animate-glow"
-          style={{
-            mixBlendMode: 'screen',
-            pointerEvents: 'none',
-            opacity: 0.5,
-          }}
-        />
       </span>
-
-      <style>
-        {`
-          @keyframes glow {
-            0% {
-              transform: translateX(-120%);
-            }
-            100% {
-              transform: translateX(120%);
-            }
-          }
-          .animate-glow {
-            animation: glow 3s infinite;
-            background-size: 200% auto;
-            will-change: transform;
-          }
-        `}
-      </style>
     </Link>
   );
 };
