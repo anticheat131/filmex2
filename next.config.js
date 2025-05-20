65367682a9ddbaa9009ca015470f44524f5d2daa
anticheat131 <wowsamowow@gmail.com>
2025-05-20 07:04:23 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,3 +5,13 @@ module.exports = {
   // Optional: helps ensure correct asset paths
   assetPrefix: '/',
 };
+module.exports = {
+  siteUrl: 'https://fmovies4u.com',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://fmovies4u.com/sitemap.xml',
+      'https://fmovies4u.com/sitemap-12.xml',
+    ],
+  },
+};
