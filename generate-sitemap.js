001fd62c680975d294c7cb95f38db67bfb45bd45
anticheat131 <wowsamowow@gmail.com>
2025-05-20 04:01:41 +0200

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const fetch = require("node-fetch");
+import fs from "fs";
+import fetch from "node-fetch";
 
 const API_KEY = process.env.TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
