09a000be92402e32e912510045bb336bab4863f2
anticheat131 <wowsamowow@gmail.com>
2025-06-15 01:37:41 +0200

    cycy

--- src/components/TrendingTodayTV.tsx
+++ src/components/TrendingTodayTV.tsx
@@ -40,7 +40,7 @@ const TrendingTodayTV = () => {
 
   return (
     <section className="w-full mt-8 p-0 m-0">
-      <div className="grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-2 gap-[2vw] w-full h-[240vw] md:h-[72vw] max-h-[140vh]">
+      <div className="grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-2 gap-[2vw] w-full h-[480vw] md:h-[72vw] max-h-[140vh]">
         {shows.map((show, idx) => (
           <div key={show.id} className="relative bg-black/80 overflow-hidden shadow-2xl flex flex-col justify-center items-center w-full h-full min-h-0 min-w-0 group flex-1 rounded-xl">
             <img
